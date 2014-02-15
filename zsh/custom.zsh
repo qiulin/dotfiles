2c1f31547a7fd436f90b68b40cf679ef5ed8d2f3
qiulin <xcodeman@gmail.com>
2014-02-15 11:55:19 +0800

    add some alias for zsh

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -2,6 +2,26 @@ alias 'cls'='clear'
 alias 'ipy'='ipython'
 alias 'py'='python'
 alias 'g'='git'
-
+alias ll='ls -l'
+alias la='ls -a'
+alias vi='vim'
+alias grep='grep --color=auto'
+alias -s html=vi
+alias -s rb=vi
+alias -s py=vi
+alias -s js=vi
+alias -s c=vi
+alias -s gz='tar -xzvf'
+alias -s tgz='tar -xzvf'
+alias -s zip='unzip'
+alias -s bz2='tar -xjvf'
 
 export PATH=$PATH:/usr/local/redis/bin/
+
+
+
+# Alias Directories
+hash -d blog='/home/xcodeman/work/devel/qiulin.github.com/_posts/'
+
+# Source autojump config file
+[[ -s ~/.autojump/etc/profile.d/autojump.zsh ]] && . ~/.autojump/etc/profile.d/autojump.zsh
